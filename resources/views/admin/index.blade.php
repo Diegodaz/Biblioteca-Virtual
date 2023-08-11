fb6970da5576a327de5881a4ca30042bf37c9adf
Diego Alonso Zanabria Sacsi <83051094+Diegodaz@users.noreply.github.com>
2023-08-11 13:29:18 -0500

    Add files via upload

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Biblioteca CS')
+
+@section('content_header')
+    <h1>Biblioteca CS</h1>
+@stop
+
+@section('content')
+    <p>Panel de administración</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Biblioteca CS')
+
+@section('content_header')
+    <h1>Listado de publicaciónes</h1>
+@stop
+
+@livewireScripts
+
+@section('content')
+    @if (session('info'))
+        <div class="alert alert-success alert-dismissable">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+            <strong>{{ session('info') }}</strong>
+        </div>
+    @endif
+    @livewire('admin.posts-index')
+@stop
